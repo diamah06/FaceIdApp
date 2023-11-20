5faef208cab766eecb4213b42769422fae21ddc8
diamah06 <tabalmahdia@yahoo.fr>
2023-11-20 14:04:04 +0100

    Initial Commit

---
+++ FaceId/FaceIdApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceIdApp.swift
+//  FaceId
+//
+//  Created by Mahdia Amriou on 20/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct FaceIdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
